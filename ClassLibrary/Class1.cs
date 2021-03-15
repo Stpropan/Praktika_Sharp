f24a49e62bd00af24d6b910a19eac6ca98f1c577
st.propan <st.propan@gmail.com>
2021-03-15 22:59:10 +0300

    Initial commit

---
+++ ClassLibrary/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Reflection;
+
+namespace ClassLibrary
+{
+    public class Quant_Uniq
+    {
+        private Regex reg_exp = new Regex("[\u002D\u0022\\.?!—,;:()\u000A]|('[^A-Za-z0-9]|[^A-Za-z0-9]')");
+
+        [UniqContent]
+        private void UniqContent(ReadTextFile txt)
+        {
+            txt = reg_exp.Replace(txt, " ");
+            txt = txt.ToLower();
+            string[] str_words = txt.Split(
+                new char[] { ' ' },
+                StringSplitOptions.RemoveEmptyEntries);
+            Quant_Uniq Aizek = new Quant_Uniq();
+            Aizek.all_words = words;
+            Aizek.uniq_words_count = new Dictionary<string, int>();
+            foreach (string element in obj.all_words)
+            {
+                if (obj.uniq_words_count.ContainsKey(element))
+                {
+                    obj.uniq_words_count[element]++;
+                }
+                else
+                {
+                    obj.uniq_words_count.Add(element, 1);
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using ClassLibrary;
+using System.Reflection;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string txt = File.ReadAllText("Azimov_Sucker-Bait.txt");
+
+            
+            
+
+            //foreach (var pair in uniq_words_count.OrderBy(pair => pair.Value))
+            //{
+            //    Console.WriteLine("{0} - {1}", pair.Key, pair.Value);
+            //}
+        }
+    }
+}
